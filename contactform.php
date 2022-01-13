98ea9ace53fcdd2c3de49121210afdd177641b9a
Ankur Patel <ankur.patel2697@gmail.com>
2022-01-13 17:19:50 +0530

    a

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail form ".$name.".\n\n".$message;
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: thankyou.html");
+}
